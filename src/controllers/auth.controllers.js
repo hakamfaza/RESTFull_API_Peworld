1f2ffee5cd0747779124f60bd10d3440ea159885
hakamfaza <hakamfaza@gmail.com>
2022-05-18 21:32:46 +0700

    add auth controller

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,13 @@
+const cryoto = require('crypto');
+const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = {
+  register: (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+  },
+};
