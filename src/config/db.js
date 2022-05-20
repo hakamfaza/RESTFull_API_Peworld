13b0d774e9bb5ab7a13264bbfe07ab2f431e4f2e
hakamfaza <hakamfaza@gmail.com>
2022-05-20 22:41:41 +0700

    update env

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,13 @@
 const { Pool } = require('pg');
 const {
-  DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER,
+  DB_NAME, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER,
 } = require('../utils/env');
 
 const db = new Pool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
-  database: DB_DATABASE,
+  database: DB_NAME,
   port: DB_PORT,
 });
 
--- src/utils/env.js
+++ src/utils/env.js
@@ -10,7 +10,7 @@ module.exports = {
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_DATABASE: process.env.DB_DATABASE,
+  DB_NAME: process.env.DB_NAME,
   DB_PORT: process.env.DB_PORT,
 
   JWT_SECRET: process.env.JWT_SECRET,
