a810ab137aa7fb5b577b058a4ca665325ea6bc43
hakamfaza <hakamfaza@gmail.com>
2022-05-23 14:34:29 +0700

    remove photo on register

--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -3,12 +3,11 @@ const {
   register, login, activation, forgot, reset,
 } = require('../controllers/auth.controllers');
 const { isVerify } = require('../middleware/authorization');
-const upload = require('../middleware/upload');
 
 const router = express.Router();
 
 router
-  .post('/register', upload, register)
+  .post('/register', register)
   .get('/activation/:token', isVerify, activation)
   .post('/login', isVerify, login)
   .put('/forgot', isVerify, forgot)
