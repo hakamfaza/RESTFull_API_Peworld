17053de2fd4546932979edb61cd2a3077b1e15f1
hakamfaza <hakamfaza@gmail.com>
2022-05-27 23:09:54 +0700

    update profile

--- src/router/users.router.js
+++ src/router/users.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {
-  getUser, getDetailUser, updateUser, deleteUser,
+  getUser, getDetailUser, updateUser, deleteUser, updateProfile,
 } = require('../controllers/users.controller');
 const upload = require('../middleware/upload');
 const jwtAuth = require('../middleware/jwtAuth');
@@ -10,7 +10,8 @@ const router = express.Router();
 router
   .get('/users', jwtAuth, getUser)
   .get('/users/:id', jwtAuth, getDetailUser)
-  .put('/users/:id', jwtAuth, upload, updateUser)
+  .put('/users/:id', jwtAuth, updateUser)
+  .put('/profile/:id', jwtAuth, upload, updateProfile)
   .delete('/users/:id', jwtAuth, deleteUser);
 
 module.exports = router;
